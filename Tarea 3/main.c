4c9519ee30e002576c5fbc8f94ca075f5c160f5e
GseitzS <gseitz@alumnos.uai.cl>
2022-05-17 17:56:35 -0400

    Reduccion Espacios

--- Tarea 3/main.c
+++ Tarea 3/main.c
@@ -1,22 +1,15 @@
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-//#include "enemigo.h"
 #include "hash.h"
 
-
 #define TABLE_SIZE 5
-
-
 
 enum OPTIONS {NOMBRE = 1, CATEGORIA = 2, PROBABILIDAD = 3};
 
 int main(int argc, char* argv[]) {
 
-
-    printf("%s\n",argv[1]);
-
-
+//printf("%s\n",argv[1]);
 
     FILE *fp = fopen(argv[1], "r");
     char line[1000];
@@ -33,15 +26,10 @@ int main(int argc, char* argv[]) {
     // segundo while para rellenar los arreglos
     while (fgets(line, 100, fp) != NULL) {
 
-
-
         char *token = strtok(line, separa);
 
         int columna = 1;
         atacante aux;
-
-
-
 
         if (token != NULL)
         {
@@ -71,8 +59,4 @@ int main(int argc, char* argv[]) {
     }
     imprimir_tabla_encadenamiento(tabla,TABLE_SIZE);
     return 0;
-
 }
-
-
-
