cb038cb475fff1acfd3798b7360ca8e73e684126
Nicolás Soto <nicolassoto1102@gmail.com>
2022-05-19 20:30:13 -0400

    Trabajo terminado + borrar funciones

--- Tarea 3/hash.h
+++ Tarea 3/hash.h
@@ -1,6 +1,5 @@
 #include "enemigo.h"
 void insertar_encadenamiento(struct node **tabla, int N, atacante key);
-int buscar_encadenamiento(struct node **tabla, int N, atacante key);
 void imprimir_tabla_encadenamiento(struct node **tabla, int N);
 void swap(struct node *nodo1, struct node *nodo2);
 void ordenar_lista(struct node *lista);
@@ -8,4 +7,3 @@ void ordenar_lista_Alfabetico(struct node *lista);
 int largo_hash(struct node *lista);
 int largo_hash_con_prob(struct node *lista);
 struct node * ordenamiento_categoria_3(struct node *lista);
-void insertar_elemento(struct node **ref_lista, atacante valor);
