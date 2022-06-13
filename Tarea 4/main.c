3217606c2cdf433c98646be977e06b85100fb665
GseitzS <gseitz@alumnos.uai.cl>
2022-06-13 12:18:23 -0400

    falta solo imprimir los N enemigos por pantalla

--- Tarea 4/main.c
+++ Tarea 4/main.c
@@ -2,14 +2,13 @@
 #include <string.h>
 #include <stdlib.h>
 #include "arbol_AVL.h"
+
 enum OPTIONS {NOMBRE = 1, CATEGORIA = 2, PROBABILIDAD = 3};
 
 int main(int argc, char* argv[]) {
 
     struct node *root = NULL;
 
-
-    printf("archivo = %s \n",argv[1]);
     // Se abre el archivo que es entregado por el usuario y se crean variables que utilizaremos para leer los datos del archivo.
     FILE *fp = fopen(argv[1], "r");
     char line[1000];
@@ -50,66 +49,23 @@ int main(int argc, char* argv[]) {
             }
 
             root = insert(root, aux);
-//            printf("Kerny = %s",insert(root, aux)->x.nombre);
-
         }
     }
 
     // Se cierra el archivo, puesto que, los datos presentes en el archivo ya fueron ingresados.
-
-    show_nodes_inorder(root);
     fclose(fp);
-
-
-    struct node *contador = root;
-
-    while(contador->right != NULL){
-        contador = contador->right;
-    }
-    printf("CONNEY = %s",contador->x.nombre);
-
-
-
-
-
-
-
-
-
-
-
-
-
 
     // Esta parte es para imprimir en el archivo de salida los nombres de los N mas peligrosos.
 
     // int contador = 0;
-    //  FILE *fpout = fopen(argv[3], "w");
-    //  int N = atoi(argv[2]);
-
-    // //Para cada categoria, comenzando con la 5 (mas peligrosos).
-    // for (int i = TABLE_SIZE -1 ; i >= 0; i--) { 
-    //     struct node *it = tabla[i];
-
-    //      for (int j = 0; j < largo ; j++){
-        
-    //     // Mientras contador sea menor a N, debemos seguir ingresando nombres al archivo de salida.
-    //     if (contador < N){
-        
-    //       fprintf(fpout, "%s\n",);
-    //       it = it->next;
-    //       contador++;
-    //      }
-
-    //     // Una vez que contador es mayor que N, ya debemos dejar de ingresar nombres. 
-    //     else if (contador > N){
-    //          break;
-    //      }
-    //    }
-    //  }
-
-    // // Se cierra el archivo de salida.
-    // fclose(fpout);
+    int N = atoi(argv[2]);
     
-    return 0;
+    int Contador = 0;
+    show_nodes(root);
+    // printf("%d", Contador);
+
+    // while (contador < N){
+    //     show_nodes(root);
+    //     contador = contador + 1;
+    // }
 }
